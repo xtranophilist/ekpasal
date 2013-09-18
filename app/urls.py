6edb264b58cc9b634967c2e122491900acc424b9
Dipesh Acharya <xtranophilist@gmail.com>
2013-09-19 02:17:09 +0545

    index, login  and logout view

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from users import views as users_views
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'app.views.home', name='home'),
-    # url(r'^app/', include('app.foo.urls')),
+                       # Examples:
+                       url(r'^$', users_views.index, name='home'),
+                       # url(r'^app/', include('app.foo.urls')),
 
-    url(r'^goms-admin/', include(admin.site.urls)),
+                       url(r'^goms-admin/', include(admin.site.urls)),
 )
--- users/views.py
+++ users/views.py
@@ -1,1 +1,29 @@
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import login
+from django.contrib.auth import logout as auth_logout
+
+
+def index(request):
+    if request.user.is_authenticated():
+        print request.user
+        return render(request, 'dashboard_index.html')
+    return login(request)
+
+
+def web_login(request, **kwargs):
+    if request.user.is_authenticated():
+        return redirect('/', **kwargs)
+    else:
+        if request.method == 'POST':
+            if request.POST.has_key('remember_me'):
+                request.session.set_expiry(1209600) # 2 weeks
+            else:
+                request.session.set_expiry(0)
+        return login(request, **kwargs)
+
+
+def logout(request, next_page=None):
+    auth_logout(request)
+    if next_page:
+        return redirect(next_page)
+    return redirect('/')
