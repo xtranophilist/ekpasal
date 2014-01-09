1e81a53dbd6ef0b50d9e3c32c327cface988af45
Dipesh Acharya <xtranophilist@gmail.com>
2014-01-09 21:35:35 +0545

    product view basic

--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from store.models import Category
+from store.models import Category, Product
 
 def list_categories(request):
     categories = Category.objects.all()
@@ -8,3 +8,8 @@ def list_categories(request):
 def view_category(request, slug):
     category = Category.objects.get(slug=slug)
     return render(request, 'view_category.html', {'category': category})
+
+
+def view_product(request, slug):
+    product = Product.objects.get(slug=slug)
+    return render(request, 'view_product.html', {'product': product})
