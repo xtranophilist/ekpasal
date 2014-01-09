69d49dfa66235734568f2f9115e4f06142884efe
Dipesh Acharya <xtranophilist@gmail.com>
2014-01-09 21:35:25 +0545

    get_starting_price template filter

--- store/templatetags/custom.py
+++ store/templatetags/custom.py
@@ -1,1 +1,16 @@
-__author__ = 'xtranophilist'
+from django.template import Library
+from django import template
+register = Library()
+
+@register.filter
+def get_starting_price(objects):
+    if len(objects) == 1:
+        return objects[0].price
+    starting_price = objects[0].price
+    iterables = iter(objects)
+    next(iterables)
+    for obj in iterables:
+        if obj.price < starting_price:
+            starting_price = obj.price
+    return 'Starting from '+ str(starting_price)
+
