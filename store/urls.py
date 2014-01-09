77f618c59e5a8efefcf95555becb46ff2d072831
Dipesh Acharya <xtranophilist@gmail.com>
2014-01-09 13:13:45 +0545

    basic viewing products by category

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,6 @@ from store import views
 
 urlpatterns = patterns('',
                        url(r'^categories/$', views.list_categories, name='list_categories'),
+                       url(r'^category/(?P<slug>[a-zA-Z0-9_.-]+)$', views.view_category, name='view_category'),
 
 )
--- store/views.py
+++ store/views.py
@@ -4,3 +4,7 @@ from store.models import Category
 def list_categories(request):
     categories = Category.objects.all()
     return render(request, 'list_categories.html', {'categories': categories})
+
+def view_category(request, slug):
+    category = Category.objects.get(slug=slug)
+    return render(request, 'view_category.html', {'category': category})
