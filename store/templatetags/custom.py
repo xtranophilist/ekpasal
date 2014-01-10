21ffcf0bc59b9affb40f3c0b935a74eea6dd1448
Dipesh Acharya <xtranophilist@gmail.com>
2014-01-10 14:09:41 +0545

    jsonify and json dump handler

--- store/templatetags/custom.py
+++ store/templatetags/custom.py
@@ -1,6 +1,11 @@
+import json
+from django.db.models import Model
 from django.template import Library
 from django import template
+from django.utils.safestring import mark_safe
+
 register = Library()
+
 
 @register.filter
 def get_starting_price(objects):
@@ -12,7 +17,8 @@ def get_starting_price(objects):
     for obj in iterables:
         if obj.price < starting_price:
             starting_price = obj.price
-    return 'Starting from '+ str(starting_price)
+    return 'Starting from ' + str(starting_price)
+
 
 @register.filter
 def parse_availability(availability):
@@ -22,3 +28,28 @@ def parse_availability(availability):
         return 'Out of Stock'
     else:
         return availability
+
+
+def handler(obj):
+    from django.db.models.fields.files import ImageFieldFile
+    if hasattr(obj, 'isoformat'):
+        return obj.isoformat()
+    elif isinstance(obj, Model):
+        model_dict = object.__dict__
+        del model_dict['_state']
+        return mark_safe(json.dumps(model_dict))
+    elif isinstance(obj, ImageFieldFile):
+        return obj.url
+    else:
+        raise TypeError, 'Object of type %s with value of %s is not JSON serializable' % (type(obj), repr(obj))
+
+
+@register.filter
+def jsonify(object):
+    # if isinstance(object, QuerySet):
+    #     return serializers.serialize('json', object)
+    if isinstance(object, Model):
+        model_dict = object.__dict__
+        del model_dict['_state']
+        return mark_safe(json.dumps(model_dict))
+    return mark_safe(json.dumps(object, default=handler))
